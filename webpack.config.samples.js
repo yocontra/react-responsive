8380bd91c3fbb73957f7410bd2512266f7c7053a
Matt Thompson <whatknight@gmail.com>
2017-09-14 15:12:54 -0400

    update webpack

--- webpack.config.samples.js
+++ webpack.config.samples.js
@@ -1,13 +1,19 @@
 const path = require('path')
+const webpack = require('webpack')
 const conf = require('./webpack.config')
 delete conf.externals
-conf.entry = [ './samples/sandbox/src/index.jsx', 'webpack-dev-server/client?http://0.0.0.0:3333', 'webpack/hot/only-dev-server' ]
+conf.entry = './samples/sandbox/src/index.js'
 conf.output = {
   path: path.join(__dirname, 'serve'),
   filename: 'sample.js'
 }
+conf.devServer = {
+  compress: true,
+  hot: true,
+  disableHostCheck: true
+}
+conf.plugins = [...conf.plugins,  new webpack.HotModuleReplacementPlugin()]
 conf.cache = true
-conf.debug = true
-conf.devtool = 'source-map'
+conf.devtool = 'inline-source-map'
 
 module.exports = conf
