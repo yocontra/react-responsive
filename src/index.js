86b2aef15a1fc2a3c7f2c31e86be019e24c89416
Herdismaria <herdis@aranja.com>
2019-01-25 15:39:18 +0100

    Fix forceStatic always evaluating to false. Store forceStatic in state and check if it changes between renders for server side rendering.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import matchMedia from 'matchmediaquery'
-import hyphenate  from 'hyphenate-style-name'
+import hyphenate from 'hyphenate-style-name'
 import mediaQuery from './mediaQuery'
-import toQuery  from './toQuery'
+import toQuery from './toQuery'
 
 const defaultTypes = {
   component: PropTypes.node,
   query: PropTypes.string,
   values: PropTypes.shape(mediaQuery.matchers),
-  children: PropTypes.oneOfType([ PropTypes.node, PropTypes.func ]),
-  onChange: PropTypes.func
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  onChange: PropTypes.func,
 }
 
 const excludedQueryKeys = Object.keys(defaultTypes)
@@ -21,44 +21,46 @@ const omit = (object, keys) => {
   return newObject
 }
 
-const getValues = ({ values={} }) =>
+const getValues = ({ values = {} }) =>
   Object.keys(values).reduce((result, key) => {
     result[hyphenate(key)] = values[key]
     return result
   }, {})
 
-const getQuery = (props) =>
-  props.query || toQuery(omit(props, excludedQueryKeys))
+const getQuery = props => props.query || toQuery(omit(props, excludedQueryKeys))
 
 const createMatchMedia = (props, query) => {
   const values = getValues(props)
-  const forceStatic = values.length === 0
+  const forceStatic = Object.keys(values).length !== 0
   return matchMedia(query, values, forceStatic)
 }
 
 class MediaQuery extends React.Component {
   static displayName = 'MediaQuery'
   static defaultProps = {
-    values: {}
+    values: {},
   }
 
   static getDerivedStateFromProps(props, state) {
     const query = getQuery(props)
     if (!query) throw new Error('Invalid or missing MediaQuery!')
-    if (query === state.query) return null
+    const forceStatic = Object.keys(props.values).length !== 0
+    if (query === state.query && forceStatic === state.forceStatic) return null
 
     const mq = createMatchMedia(props, query)
     return {
       matches: mq.matches,
       mq,
-      query
+      query,
+      forceStatic,
     }
   }
 
   state = {
     matches: false,
     mq: null,
-    query: ''
+    query: '',
+    forceStatic: false,
   }
 
   componentDidMount() {
@@ -85,7 +87,7 @@ class MediaQuery extends React.Component {
     this.cleanupMediaQuery(this.state.mq)
   }
 
-  cleanupMediaQuery = (mq) => {
+  cleanupMediaQuery = mq => {
     if (!mq) return
     mq.removeListener(this.updateMatches)
     mq.dispose()
@@ -105,7 +107,4 @@ class MediaQuery extends React.Component {
   }
 }
 
-export {
-  MediaQuery as default,
-  toQuery
-}
+export { MediaQuery as default, toQuery }
