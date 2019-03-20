8c4d50b02babbebae1477670d6db78ecae8c7793
Brandon Coffman <brandon.coffman@gmail.com>
2019-03-19 23:36:29 -0400

    Use correct react globals

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,18 @@ module.exports = {
   },
   devtool: 'source-map',
   externals: {
-    'react': 'umd react',
-    'react-dom': 'umd react-dom'
+    'react': {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
   },
   plugins,
   resolve: {
