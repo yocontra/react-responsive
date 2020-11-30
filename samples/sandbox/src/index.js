0aa106019c991d0db29b01a927f1d88f4af826ff
contra <yo@contra.io>
2020-11-30 12:57:57 -0700

    chore: auto update sample page

--- samples/sandbox/src/index.js
+++ samples/sandbox/src/index.js
@@ -7,27 +7,20 @@ window.React = React // for dev
 const App = () =>
   <div>
     <div className="header">Device Test! Below are some guesses.</div>
-    <MediaQuery minDeviceWidth={1224}>
-      <div>You are a desktop or laptop (min-device-width: 1224px)</div>
-      <MediaQuery minDeviceWidth={1824}>
-        <div>You also have a huge screen (min-device-width: 1824px)</div>
-      </MediaQuery>
-      <MediaQuery maxWidth={1224}>
-        <div>You are sized like a tablet or mobile phone (max-width: 1224px)</div>
-      </MediaQuery>
-    </MediaQuery>
-    <MediaQuery maxDeviceWidth={1224}>
-      <div>You are a tablet or mobile phone (max-device-width: 1224px)</div>
-    </MediaQuery>
+    <MediaQuery minWidth={1224}>{(matches) =>
+      matches
+        ? <div className="item">You are a desktop or laptop (min-width: 1224px)</div>
+        : <div className="item">You are sized like a tablet or mobile phone (max-width: 1224px)</div>
+    }</MediaQuery>
 
     <MediaQuery orientation="portrait">
-      <div>You are portrait (orientation: portrait)</div>
+      <div className="item">You are portrait (orientation: portrait)</div>
     </MediaQuery>
     <MediaQuery orientation="landscape">
-      <div>You are landscape (orientation: landscape)</div>
+      <div className="item">You are landscape (orientation: landscape)</div>
     </MediaQuery>
     <MediaQuery minResolution="2dppx">
-      <div>You are retina (min-resolution: 2dppx)</div>
+      <div className="item">You are retina (min-resolution: 2dppx)</div>
     </MediaQuery>
   </div>
 
